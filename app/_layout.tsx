57cfaa26503551f7774eb401adbd689f72b12f2a
jshaine <90233510+jshainesantos@users.noreply.github.com>
2026-04-23 18:22:06 +0800

    update layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,24 @@
-import { Stack } from "expo-router";
 import "../global.css";
+import { Stack } from "expo-router";
+import { CartProvider } from "./context/CartContext";
 
-export default function RootLayout() {
-  return <Stack />;
+export default function Layout() {
+  return (
+    <CartProvider>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            title: "Products",
+          }}
+        />
+        <Stack.Screen
+          name="cart"
+          options={{
+            headerTitle: "",
+          }}
+        />
+      </Stack>
+    </CartProvider>
+  );
 }
