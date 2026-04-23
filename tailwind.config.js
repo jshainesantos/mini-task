93157f255e5e33ef948392a3c1a715e3a4671478
jshaine <90233510+jshainesantos@users.noreply.github.com>
2026-04-23 18:19:05 +0800

    feat(theme): extend Tailwind colors with brand and custom palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,16 @@ module.exports = {
   ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: "#ed5145",
+        "deep-black": "#1f1f1f",
+        "mid-gray": "#353535",
+        magenta: "#b92d71",
+        danger: "#fa1b1b",
+        "brand-dark": "#B92D61",
+      },
+    },
   },
   plugins: [],
 };
