7b31734acf82ea28bc5f5215086d73350568b843
jshaine <90233510+jshainesantos@users.noreply.github.com>
2026-04-23 18:23:49 +0800

    feat(types): add CartItem, Product, and Voucher type definitions

---
+++ app/types/Product.ts
@@ -0,0 +1,6 @@
+export type Product = {
+  id: number;
+  productName: string;
+  description: string;
+  price: number;
+};
---
+++ app/types/Voucher.ts
@@ -0,0 +1,4 @@
+export type Voucher = {
+  code: string;
+  discountPercentage: number;
+};
